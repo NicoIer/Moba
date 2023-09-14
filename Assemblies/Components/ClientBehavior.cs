b5eb1b0f7d4c6a26baa04796383956432d404c33
NicoIer <NicoIer@163.com>
2023-09-14 21:23:31 +0800

    feat: init

---
+++ Assemblies/Components/ClientBehavior.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+
+namespace Nico
+{
+    public abstract partial class ClientBehavior : MonoBehaviour, IClientBehavior
+    {
+        public uint netObjId { get; set; }
+        public uint behaviorId { get; set; }
+        public ClientObj clientObj { get; set; }
+
+        public void OnConnect(int channelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnConnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDisconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+
+#if UNITY_EDITOR
+
+        private void OnValidate()
+        {
+            // parents中必须有一个NetGameObject
+            Transform cur = transform;
+            while (cur != null)
+            {
+                if (cur.GetComponent<NetGameObject>() != null)
+                {
+                    return;
+                }
+
+                cur = cur.parent;
+            }
+
+            Debug.LogError($"{nameof(ClientBehavior)} must have a {nameof(NetGameObject)} in parents");
+        }
+
+#endif
+    }
+}
