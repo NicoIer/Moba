00f9f7064628985203af419d068b34910bcdcd13
NicoIer <NicoIer@163.com>
2023-09-19 20:21:08 +0800

    fix: errors

--- Assemblies/ClientTest.cs
+++ Assemblies/ClientTest.cs
@@ -1,53 +1,53 @@
-using System;
-using Google.Protobuf.Reflection;
-using Nico;
-using Sirenix.OdinInspector;
-using UnityEngine;
-
-namespace MOBA
-{
-    public class ClientTest: MonoBehaviour
-    {
-        private void Start()
-        {
-            kcp2k.Log.Info = Debug.Log;
-            // ClientManager.singleton.client.RegisterHandler<CreateObjResponse>(OnCreateNetObj);
-        }
-
-        [Button]
-        public void StartClient()
-        {
-            ClientManager.singleton.NetStart();
-        }
-
-        [Button]
-        public void StopClient()
-        {
-            ClientManager.singleton.NetStop();
-        }
-
-        // [Button]
-        // public void CreateObj(Vector3 pos)
-        // {
-        //     CreateObjRequest request = ProtoHandler.Get<CreateObjRequest>();
-        //     ProtoVector3 protoVector3 = ProtoHandler.Get<ProtoVector3>();
-        //     protoVector3.X = pos.x;
-        //     protoVector3.Y = pos.y;
-        //     protoVector3.Z = pos.z;
-        //     request.Pos = protoVector3;
-        //     ClientManager.singleton.Send(request);
-        // }
-        // public void OnCreateNetObj(ServerMsg<CreateObjResponse> response)
-        // {
-        //     CreateObjResponse msg = response.msg;
-        //     Debug.Log($"OnCreateNetObj,pos:{response.msg.Pos}");
-        //     GameObject netObj = new GameObject($"NetObj:{msg.ObjId}")
-        //     {
-        //         transform =
-        //         {
-        //             position = new UnityEngine.Vector3(msg.Pos.X, msg.Pos.Y, msg.Pos.Z)
-        //         }
-        //     };
-        // }
-    }
-}
+// using System;
+// using Google.Protobuf.Reflection;
+// using Nico;
+// using Sirenix.OdinInspector;
+// using UnityEngine;
+//
+// namespace MOBA
+// {
+//     public class ClientTest: MonoBehaviour
+//     {
+//         private void Start()
+//         {
+//             kcp2k.Log.Info = Debug.Log;
+//             // ClientManager.singleton.client.RegisterHandler<CreateObjResponse>(OnCreateNetObj);
+//         }
+//
+//         [Button]
+//         public void StartClient()
+//         {
+//             ClientManager.singleton.NetStart();
+//         }
+//
+//         [Button]
+//         public void StopClient()
+//         {
+//             ClientManager.singleton.NetStop();
+//         }
+//
+//         // [Button]
+//         // public void CreateObj(Vector3 pos)
+//         // {
+//         //     CreateObjRequest request = ProtoHandler.Get<CreateObjRequest>();
+//         //     ProtoVector3 protoVector3 = ProtoHandler.Get<ProtoVector3>();
+//         //     protoVector3.X = pos.x;
+//         //     protoVector3.Y = pos.y;
+//         //     protoVector3.Z = pos.z;
+//         //     request.Pos = protoVector3;
+//         //     ClientManager.singleton.Send(request);
+//         // }
+//         // public void OnCreateNetObj(ServerMsg<CreateObjResponse> response)
+//         // {
+//         //     CreateObjResponse msg = response.msg;
+//         //     Debug.Log($"OnCreateNetObj,pos:{response.msg.Pos}");
+//         //     GameObject netObj = new GameObject($"NetObj:{msg.ObjId}")
+//         //     {
+//         //         transform =
+//         //         {
+//         //             position = new UnityEngine.Vector3(msg.Pos.X, msg.Pos.Y, msg.Pos.Z)
+//         //         }
+//         //     };
+//         // }
+//     }
+// }
--- Assemblies/ServerTest.cs
+++ Assemblies/ServerTest.cs
@@ -1,60 +1,60 @@
-using System;
-using System.Collections.Generic;
-using Nico;
-using Sirenix.OdinInspector;
-using UnityEngine;
-
-namespace MOBA
-{
-    public class ServerTest : MonoBehaviour
-    {
-        private void Start()
-        {
-            kcp2k.Log.Info = Debug.Log;
-            // ServerManager.singleton.server.onConnected += (connectId) =>
-            // {
-            //     Debug.Log($"connectId:{connectId} connected");
-            // };
-            //
-            // ServerManager.singleton.server.onDisconnected += (connectId) =>
-            // {
-            //     Debug.Log($"connectId:{connectId} disconnected");
-            // };
-
-            ServerManager.singleton.NetStart();
-            ServerManager.singleton.server.Listen<CreateObjRequest>(OnCreateNetObj);
-            ServerManager.singleton.server.Listen<MoveRequest>(OnMoveRequest);
-        }
-
-        private void OnDestroy()
-        {
-            ServerManager.singleton.server.UnListen<CreateObjRequest>(OnCreateNetObj);
-            ServerManager.singleton.server.UnListen<MoveRequest>(OnMoveRequest);
-        }
-
-        Dictionary<uint, GameObject> _netObjs = new Dictionary<uint, GameObject>();
-        public uint nextId = 0;
-
-        public void OnCreateNetObj(ClientMsg<CreateObjRequest> request)
-        {
-            CreateObjRequest msg = request.msg;
-            GameObject netObj = new GameObject($"NetObj:{nextId}")
-            {
-                transform =
-                {
-                    position = new UnityEngine.Vector3(msg.Pos.X, msg.Pos.Y, msg.Pos.Z)
-                }
-            };
-            _netObjs[nextId] = netObj;
-            ++nextId;
-            CreateObjResponse response = ProtoHandler.Get<CreateObjResponse>();
-            response.ObjId = nextId;
-            response.Pos = msg.Pos;
-            ServerManager.singleton.Send(request.connectId, response);
-        }
-
-        public void OnMoveRequest(ClientMsg<MoveRequest> request)
-        {
-        }
-    }
-}
+// using System;
+// using System.Collections.Generic;
+// using Nico;
+// using Sirenix.OdinInspector;
+// using UnityEngine;
+//
+// namespace MOBA
+// {
+//     public class ServerTest : MonoBehaviour
+//     {
+//         private void Start()
+//         {
+//             kcp2k.Log.Info = Debug.Log;
+//             // ServerManager.singleton.server.onConnected += (connectId) =>
+//             // {
+//             //     Debug.Log($"connectId:{connectId} connected");
+//             // };
+//             //
+//             // ServerManager.singleton.server.onDisconnected += (connectId) =>
+//             // {
+//             //     Debug.Log($"connectId:{connectId} disconnected");
+//             // };
+//
+//             ServerManager.singleton.NetStart();
+//             ServerManager.singleton.server.Listen<CreateObjRequest>(OnCreateNetObj);
+//             ServerManager.singleton.server.Listen<MoveRequest>(OnMoveRequest);
+//         }
+//
+//         private void OnDestroy()
+//         {
+//             ServerManager.singleton.server.UnListen<CreateObjRequest>(OnCreateNetObj);
+//             ServerManager.singleton.server.UnListen<MoveRequest>(OnMoveRequest);
+//         }
+//
+//         Dictionary<uint, GameObject> _netObjs = new Dictionary<uint, GameObject>();
+//         public uint nextId = 0;
+//
+//         public void OnCreateNetObj(ClientMsg<CreateObjRequest> request)
+//         {
+//             CreateObjRequest msg = request.msg;
+//             GameObject netObj = new GameObject($"NetObj:{nextId}")
+//             {
+//                 transform =
+//                 {
+//                     position = new UnityEngine.Vector3(msg.Pos.X, msg.Pos.Y, msg.Pos.Z)
+//                 }
+//             };
+//             _netObjs[nextId] = netObj;
+//             ++nextId;
+//             CreateObjResponse response = ProtoHandler.Get<CreateObjResponse>();
+//             response.ObjId = nextId;
+//             response.Pos = msg.Pos;
+//             ServerManager.singleton.Send(request.connectId, response);
+//         }
+//
+//         public void OnMoveRequest(ClientMsg<MoveRequest> request)
+//         {
+//         }
+//     }
+// }
